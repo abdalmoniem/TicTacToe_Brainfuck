49117c36ecba326b0977fdf51245922d6d596c52
abdalmoniem-alhifnawy <hifnawy_moniem@hotmail.com>
2020-01-16 22:34:34 +0200

    Demo Page
    Added a demo website for users to try out the game on without having to download the source code
    
    Signed-off-by: abdalmoniem-alhifnawy <hifnawy_moniem@hotmail.com>

---
+++ functions.js
@@ -0,0 +1,93 @@
+keys=[];
+document.onkeydown=function(e){keys[e.keyCode]=true;}
+document.onkeyup=function(e) {
+ var e = e || window.event;
+ var uc= e.keyCode
+ keys[uc]=false;
+ if (e.ctrlKey) return;
+ if (uc==84) {
+  document.cookie="font=;expires="+(new Date(1).toUTCString())+";path=/;";
+  location.reload(true);
+ }
+ if (uc==190) confuzz(document.body);
+ if (uc==73) {
+  if (!window.invertedColour) {
+   invertPageColour();
+   window.sessionStorage.inverted='1';
+  }else {
+   document.head.removeChild(invertedColour);
+   invertedColour=false;
+   window.sessionStorage.inverted='';
+  }
+ }
+ if (uc==77){
+  if (!window.mirrored){
+   (mirrored=document.createElement("style")).innerHTML='body{transform:scaleX(-1);}';
+   document.head.appendChild(mirrored)
+  }else{document.head.removeChild(mirrored);mirrored=false;}
+ }
+};
+
+function invertPageColour(){
+   invertedColour=document.createElement("style");
+   invertedColour.innerHTML='body{background:#000;filter: invert(1) grayscale(1); -webkit-filter: invert(1) grayscale(1);}';
+   document.head.appendChild(invertedColour);
+}
+if (window.sessionStorage.inverted) invertPageColour();
+
+//function confuzz(obj){
+//  var a=obj.innerHTML.split(""), len=a.length, word=[], i=0, j=0, u,v,w, s;
+//
+//  while (i<len) {
+//  
+//    if (a[i].match(/[a-zA-Z]/)) word[j++]=a[i];
+//    else {
+//      if (j>2) {
+//        word=word.slice(1,j-1);
+//        
+//        w = word.length;
+//        while(w){ 
+//          u = Math.floor(Math.random() * w); 
+//          v = word[--w]; 
+//          word[w] = word[u]; 
+//          word[u] = v;
+//        }
+//        Array.prototype.splice.apply(a, [i-j+1, j-2].concat(word));
+//      }
+//      word=[];j=0;
+//    }
+//    while (a[i]=="<" || a[i]=="&") {
+//      s=a.slice(i+1,i+6).join("");
+//      if (s=="scrip" || s=="style") { while (a[++i]!="<"&&a[i+1]!="/" &&i<len) {} }
+//      
+//      if (a[i]=="<") {while (a[++i]!=">" &&i<len) {}}
+//      if (a[i]=="&") {while (a[++i]!=";" &&i<len) {}}
+//    }
+//    i++;
+//  }
+//  obj.innerHTML=a.join("");
+//}
+
+// UPDATE: This version is more confuzzling
+function confuzz() {
+  (function forEach(nl, f) { for (var i=0; i<nl.length; ++i){ f(nl[i], forEach) }})
+  (document.querySelectorAll('*'),
+    function(n, fforEach) { 
+      if ( ['script','textarea','style'].indexOf(n.tagName.toLowerCase()) +1 ) return;
+      fforEach(n.childNodes, function(n){
+        if (n.nodeName=="#text") n.textContent = n.textContent.replace(/(\w)(\w\w+)(\w)/g, function (word, _, r, $){ 
+          r = r.split('')
+          for (var j=r.length+1; --j;) {
+            var i = ~~(Math.random()*j)
+            var x=r[j]; r[j]=r[i]; r[i]=x
+          }    
+          return _ + r.join('') + $
+        })  
+      }) 
+    } 
+  )
+}
+
+//disable Firefox bfcache
+//function UnloadHandler() { window.removeEventListener('unload', UnloadHandler, false); }
+//window.addEventListener('unload', UnloadHandler, false);
